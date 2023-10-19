63637f81704a7a274660e647d85aaa66ed3af135
Code of K <korjon0000@gmail.com>
2023-10-19 17:00:25 -0400

    fixed small bug in e0

--- ai.py
+++ ai.py
@@ -19,7 +19,8 @@ class AI:
 
     def minimax(self, game_state, depth, max_player, starting_depth, rounds_left):
 
-        if depth == 0 or self.is_game_over_from_state(game_state, rounds_left):  # checking if the game is over or reached max depth
+        if depth == 0 or self.is_game_over_from_state(game_state,
+                                                      rounds_left):  # checking if the game is over or reached max depth
 
             return self.calculate_heuristic(game_state), game_state
 
@@ -49,7 +50,8 @@ class AI:
 
     def alpha_beta(self, game_state, depth, alpha, beta, max_player, starting_depth, rounds_left):
 
-        if depth == 0 or self.is_game_over_from_state(game_state, rounds_left):  # checking if the game is over or reached max depth
+        if depth == 0 or self.is_game_over_from_state(game_state,
+                                                      rounds_left):  # checking if the game is over or reached max depth
             return self.calculate_heuristic(game_state), game_state
 
         if max_player:
@@ -59,7 +61,7 @@ class AI:
                 current_eval, state = self.alpha_beta(child, depth - 1, alpha, beta, False, starting_depth, rounds_left)
 
                 if current_eval > max_eval[0]:
-                    max_eval = (current_eval, child if depth == starting_depth - 1 else None)
+                    max_eval = (current_eval, child if depth == starting_depth else None)
 
                 alpha = max(alpha, current_eval)
                 if beta <= alpha:
@@ -76,7 +78,7 @@ class AI:
                 if current_eval < min_eval[0]:
                     # Starting depth - 1 because we want to get the state
                     # Right below the current state
-                    min_eval = (current_eval, child if depth == starting_depth - 1 else None)
+                    min_eval = (current_eval, child if depth == starting_depth else None)
                 beta = min(beta, current_eval)
                 if beta <= alpha:
                     break
@@ -101,37 +103,38 @@ class AI:
 
         for row in game_state.current_state:
             for pos in row:  # pos = position
-                if pos[0] == "d":
+                if len(pos) != 0:
+                    if pos[0] == "d":
 
-                    # adding the stats to the defensive unit variables
-                    match pos[1]:
-                        case "A":
-                            d_a += 9999
+                        # adding the stats to the defensive unit variables
+                        match pos[1]:
+                            case "A":
+                                d_a += 9999
 
-                        case "P":
-                            d_p += 3
+                            case "P":
+                                d_p += 3
 
-                        case "T":
-                            d_t += 3
+                            case "T":
+                                d_t += 3
 
-                        case "F":
-                            d_f += 3
+                            case "F":
+                                d_f += 3
 
-                elif pos[0] == "a":
+                    elif pos[0] == "a":
 
-                    # adding the stats to the unit variables
-                    match pos[1]:
-                        case "A":
-                            a_a += 9999
+                        # adding the stats to the unit variables
+                        match pos[1]:
+                            case "A":
+                                a_a += 9999
 
-                        case "P":
-                            a_p += 3
+                            case "P":
+                                a_p += 3
 
-                        case "V":
-                            a_v += 3
+                            case "V":
+                                a_v += 3
 
-                        case "F":
-                            a_f += 3
+                            case "F":
+                                a_f += 3
 
         if self.belongs_to == Player.ATTACKER:
             heuristic_value = (a_a + a_p + a_v + a_f + a_t) - (d_a + d_p + d_v + d_f + d_t)
@@ -196,37 +199,38 @@ class AI:
 
         for row in game_state.current_state:
             for pos in row:  # pos = position
-                if pos[0] == "d":
+                if len(pos) != 0:
+                    if pos[0] == "d":
 
-                    # adding the stats to the defensive unit variables
-                    match pos[1]:
-                        case "A":
-                            d_a += 100
+                        # adding the stats to the defensive unit variables
+                        match pos[1]:
+                            case "A":
+                                d_a += 100
 
-                        case "P":
-                            d_p += 15
+                            case "P":
+                                d_p += 15
 
-                        case "T":
-                            d_t += 30
+                            case "T":
+                                d_t += 30
 
-                        case "F":
-                            d_f += 20
+                            case "F":
+                                d_f += 20
 
-                elif pos[0] == "a":
+                    elif pos[0] == "a":
 
-                    # adding the stats to the unit variables
-                    match pos[1]:
-                        case "A":
-                            a_a += 100
+                        # adding the stats to the unit variables
+                        match pos[1]:
+                            case "A":
+                                a_a += 100
 
-                        case "P":
-                            a_p += 20
+                            case "P":
+                                a_p += 20
 
-                        case "V":
-                            a_v += 10
+                            case "V":
+                                a_v += 10
 
-                        case "F":
-                            a_f += 30
+                            case "F":
+                                a_f += 30
 
         if self.belongs_to == Player.ATTACKER:
             heuristic_value = (a_a + a_p + a_v + a_f + a_t) - (d_a + d_p + d_v + d_f + d_t)
